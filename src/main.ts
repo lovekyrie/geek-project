841c4067ee207d9d86fa0c940cd52c4bbb2e0e15
duanyupeng <fengdurant@163.com>
2022-11-02 14:22:37 +0800

    mount occupy one line

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import Modal from './components/modal/Modal.vue'
 import './index.scss'
 import router from './router' //声明带后缀，应用也要带后缀，保持统一
 import { createPinia } from 'pinia'
-
 import { dialogInstall } from './components/modal'
+
 const app = createApp(App)
 app.component('EasyModal', Modal)
-app.use(dialogInstall).use(router).use(createPinia()).mount('#app')
+app.use(dialogInstall).use(router).use(createPinia())
+app.mount('#app')
