4ba33681bface1ae09594691b8805eab3a449221
lovekyrie <fengdurant@163.com>
2022-11-02 22:08:05 +0800

    add self transition comp

---
+++ src/components/GeekTransition.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="showDiv">按钮</button>
+  <div v-if="show"></div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const show = ref(false)
+
+function showDiv() {
+  show.value = !show.value
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  margin-left: 100px;
+  width: 100px;
+  height: 60px;
+  background-color: bisque;
+}
+</style>
